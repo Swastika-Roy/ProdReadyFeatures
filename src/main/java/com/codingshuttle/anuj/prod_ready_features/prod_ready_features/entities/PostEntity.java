a9e28b61f729bbdd43b43075e3d2f28dc668f851
Swastika Roy <swastikaroytitu@gmail.com>
2025-09-19 20:57:16 +0530

    some changes

--- src/main/java/com/codingshuttle/anuj/prod_ready_features/prod_ready_features/entities/PostEntity.java
+++ src/main/java/com/codingshuttle/anuj/prod_ready_features/prod_ready_features/entities/PostEntity.java
@@ -12,17 +12,17 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 @Table(name = "posts")
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Getter
-//@Setter
+
 @Audited
 public class PostEntity extends AuditableEntity{
 
-    //    // Getters and Setters
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
@@ -35,38 +35,14 @@ public class PostEntity extends AuditableEntity{
     public PostEntity() {
     }
 
-    // All-args constructor (for convenience)
+
     public PostEntity(Long id, String title, String description) {
         this.id = id;
         this.title = title;
         this.description = description;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-//    @PrePersist
+    //    @PrePersist
 //    void beforeSave(){
 //
 //    }
